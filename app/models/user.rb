c2a699342e1b8cc3767b82b8d1293800b16fcf44
Justin Bean <beanjustinr@gmail.com>
2015-11-03 15:42:29 -0500

    devise username set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username,
+            :presence => true,
+            :uniqueness => {
+            :case_sensitive => false
+          }
+
+  validate :validate_username
+
+  attr_accessor :login
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_hash).first
+    end
+  end
+
 end
